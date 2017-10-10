a8b57b374824dcdd5087857192e4045995d7b395
Ariel <raarielgrace@gmail.com>
2017-10-09 17:06:43 -0700

    added west coast op mode abstract class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastOpMode.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+    import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+    import com.qualcomm.robotcore.hardware.DcMotor;
+/**
+ * Created by Alex and Brian on 10/9/2017.
+ *
+ * abstract class to provide west coast movement methods.
+ *
+ */
+
+public abstract class WestCoastOpMode extends OpMode{
+
+    public DcMotor MR1;
+    public DcMotor MR2;
+    public DcMotor ML1;
+    public DcMotor ML2;
+
+    
+}
