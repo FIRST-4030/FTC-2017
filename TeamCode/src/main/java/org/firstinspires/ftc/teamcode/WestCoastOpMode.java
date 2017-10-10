535c61925562257d4aac5a65af11580af8577734
Ariel <raarielgrace@gmail.com>
2017-10-09 17:35:09 -0700

    added west coast op mode abstract class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastOpMode.java
@@ -11,10 +11,32 @@ package org.firstinspires.ftc.teamcode;
 
 public abstract class WestCoastOpMode extends OpMode{
 
-    public DcMotor MR1;
-    public DcMotor MR2;
-    public DcMotor ML1;
-    public DcMotor ML2;
+    public DcMotor LWheel1;
+    public DcMotor LWheel2;
+    public DcMotor RWheel1;
+    public DcMotor RWheel2;
 
-    
+    public void init() {
+        LWheel1 = hardwareMap.dcMotor.get("ML1");
+        LWheel2 = hardwareMap.dcMotor.get("ML2");
+        RWheel1 = hardwareMap.dcMotor.get("MR1");
+        RWheel2 = hardwareMap.dcMotor.get("MR2");
+    }
+
+    /**
+     * Turn a DC motor at a power range of -1 to 1
+     *
+     * Throws an IllegalArgumentException if the power parameter is not between -1 and 1 inclusive
+     *
+     * @param motor The motor to turn
+     * @param power The power to turn the motor at. Between -1 and 1
+     */
+
+    public void turnMotor(DcMotor motor, double power) {
+        if (Math.abs(power) > 1) {
+            throw new IllegalArgumentException("turnmotor: the power value must be between -1 and 1 inclusive");
+        }
+
+        motor.setPower(power);
+    }
 }
