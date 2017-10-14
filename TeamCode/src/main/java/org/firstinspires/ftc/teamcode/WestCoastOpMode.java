70625451591bb0d34771291fd2c23206eeebc349
robotics <ihsrobotics4030@gmail.com>
2017-10-13 18:25:40 -0700

    finished West Coast except for servo position values and lift stops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastOpMode.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-    import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-    import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.*;
 /**
  * Created by Alex and Brian on 10/9/2017.
  *
@@ -11,18 +11,40 @@ package org.firstinspires.ftc.teamcode;
 
 public abstract class WestCoastOpMode extends OpMode{
 
-    public DcMotor LWheel1;
-    public DcMotor LWheel2;
-    public DcMotor RWheel1;
-    public DcMotor RWheel2;
-    public DcMotor Lift;
+    public final int LEFT = 0;
+    public final int RIGHT = 1;
+
+    public final double UPPER_CLAW_MAX = 1.2;
+    public final double UPPER_CLAW_MIN = .2;
+    public final double LOWER_CLAW_MAX = 1.2;
+    public final double LOWER_CLAW_MIN = .2;
+
+    public DcMotor lWheel1;
+    public DcMotor lWheel2;
+    public DcMotor rWheel1;
+    public DcMotor rWheel2;
+    public DcMotor lift;
+    public Servo topClaw;
+    public Servo bottomClaw;
 
     public void init() {
-        LWheel1 = hardwareMap.dcMotor.get("ML1");
-        LWheel2 = hardwareMap.dcMotor.get("ML2");
-        RWheel1 = hardwareMap.dcMotor.get("MR1");
-        RWheel2 = hardwareMap.dcMotor.get("MR2");
-        Lift = hardwareMap.dcMotor.get("LM1");
+        // Set Up hardware
+        lWheel1 = hardwareMap.dcMotor.get("ML1");
+        lWheel2 = hardwareMap.dcMotor.get("ML2");
+        rWheel1 = hardwareMap.dcMotor.get("MR1");
+        rWheel2 = hardwareMap.dcMotor.get("MR2");
+        lift = hardwareMap.dcMotor.get("LM1");
+        topClaw = hardwareMap.servo.get("CL1");
+        bottomClaw = hardwareMap.servo.get("CL2");
+
+        topClaw.setDirection(Servo.Direction.REVERSE);
+
+        topClaw.scaleRange(UPPER_CLAW_MIN, UPPER_CLAW_MAX);
+        bottomClaw.scaleRange(LOWER_CLAW_MIN, LOWER_CLAW_MAX);
+
+        setServoPosition(topClaw, UPPER_CLAW_MIN);
+        setServoPosition(bottomClaw, LOWER_CLAW_MIN);
+
     }
 
     /**
@@ -40,11 +62,41 @@ public abstract class WestCoastOpMode extends OpMode{
         }
 
         motor.setPower(power);
+
     }
 
     public void liftMotor(DcMotor liftMotor, double power){
         if (Math.abs(power) > 1) {
             throw new IllegalArgumentException("liftMotor: the power value must be between -1 and 1 inclusive");
         }
+
+        liftMotor.setPower(power);
+
     }
+
+    public void setServoPosition(Servo servo, double position){
+
+        servo.setPosition(position);
+
+    }
+
+    public void setSidePower(int side, double power){
+
+        switch(side){
+        case LEFT:
+
+            turnMotor(lWheel1, -power);
+            turnMotor(lWheel2, -power);
+
+            break;
+        case RIGHT:
+
+            turnMotor(rWheel1, power);
+            turnMotor(rWheel2, power);
+
+            break;
+        }
+
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastTeleOpMode1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastTeleOpMode1.java
@@ -5,11 +5,70 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  * Created by Alex Wang on 10/9/2017.
  */
 
+@TeleOp(name="WestCoastTeleOpMode1", group="WestCoastOpMode")
 public class WestCoastTeleOpMode1 extends WestCoastOpMode{
 
     @Override
     public void loop()
     {
 
+        moveBase();
+
+        moveClaws();
+
+        moveLift();
+
+        telemetry.addData("Top:", topClaw.getPosition());
+        telemetry.addData("Bottom:", bottomClaw.getPosition());
+        telemetry.update();
+
     }
+
+    public void moveLift(){
+
+        liftMotor(lift, gamepad1.right_stick_y);
+
+    }
+
+    public void moveClaws(){
+
+        if(gamepad1.a) setServoPosition(bottomClaw, LOWER_CLAW_MIN);
+        else if(gamepad1.b) setServoPosition(bottomClaw, LOWER_CLAW_MAX);
+
+        if(gamepad1.x) setServoPosition(topClaw, UPPER_CLAW_MIN);
+        else if(gamepad1.y) setServoPosition(topClaw, UPPER_CLAW_MAX);
+
+    }
+
+    public void moveBase(){
+
+        if(gamepad1.dpad_down){
+
+            setSidePower(RIGHT, -1);
+            setSidePower(LEFT, -1);
+
+        } else if(gamepad1.dpad_right){
+
+            setSidePower(RIGHT, -1);
+            setSidePower(LEFT, 1);
+
+        } else if(gamepad1.dpad_up){
+
+            setSidePower(RIGHT, 1);
+            setSidePower(LEFT, 1);
+
+        } else if(gamepad1.dpad_left){
+
+            setSidePower(RIGHT, 1);
+            setSidePower(LEFT, -1);
+
+        } else {
+
+            setSidePower(RIGHT, 0);
+            setSidePower(LEFT, 0);
+
+        }
+
+    }
+
 }
