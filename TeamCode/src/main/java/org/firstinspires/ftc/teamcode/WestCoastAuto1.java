feb19b75c1c6c3df39edda2f590a9fcddc34ea30
robotics <ihsrobotics4030@gmail.com>
2017-10-14 12:05:19 -0700

    added empty auto program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastAuto1.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by robotics on 10/14/2017.
+ */
+
+public class WestCoastAuto1 extends WestCoastOpMode{
+
+    @Override
+    public void loop() {
+
+        
+
+    }
+
+}
