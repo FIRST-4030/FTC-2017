622346c4878daf508e5da242a1ff9be0966227c0
Zachary Kotlarek <zach@kotlarek.com>
2017-11-01 17:00:00 -0700

    Config for non-drive motors and servos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/MotorConfigs.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.config;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.actuators.Motor;
+import org.firstinspires.ftc.teamcode.actuators.MotorConfig;
+
+public class MotorConfigs {
+    private static BOT bot = null;
+
+    public Motor init(HardwareMap map, Telemetry telemetry, String name) {
+        Motor motor = null;
+        for (BOT i : BOT.values()) {
+            bot = i;
+            motor = new Motor(map, config(name));
+            if (motor.isAvailable()) {
+                if (bot.ordinal() != 0) {
+                    telemetry.log().add("NOTICE: Using " + name + " motor config " + bot);
+                }
+                break;
+            }
+        }
+        assert motor != null;
+        if (!motor.isAvailable()) {
+            telemetry.log().add("ERROR: Unable to initialize motor: " + name);
+        }
+        return motor;
+    }
+
+    private MotorConfig config(String name) {
+        MotorConfig config = null;
+        assert bot != null;
+        switch (bot) {
+            case FINAL:
+                config = FinalBot(name);
+                break;
+            case CODE:
+                config = CodeBot(name);
+                break;
+        }
+        return config;
+    }
+
+    private static MotorConfig CodeBot(String name) {
+        MotorConfig config = null;
+        switch (name) {
+            default:
+                break;
+        }
+        return config;
+    }
+
+    private static MotorConfig FinalBot(String name) {
+        MotorConfig config = null;
+        switch (name) {
+            case "LIFT":
+                config = new MotorConfig("LM1", false);
+                break;
+        }
+        return config;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ServoConfigs.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.config;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.actuators.ServoFTC;
+import org.firstinspires.ftc.teamcode.actuators.ServoFTCConfig;
+
+public class ServoConfigs {
+    private static BOT bot = null;
+
+    public ServoFTC init(HardwareMap map, Telemetry telemetry, String name) {
+        ServoFTC servo = null;
+        for (BOT i : BOT.values()) {
+            bot = i;
+            servo = new ServoFTC(map, config(name));
+            if (servo.isAvailable()) {
+                if (bot.ordinal() != 0) {
+                    telemetry.log().add("NOTICE: Using " + name + " servo config " + bot);
+                }
+                break;
+            }
+        }
+        assert servo != null;
+        if (!servo.isAvailable()) {
+            telemetry.log().add("ERROR: Unable to initialize servo: " + name);
+        }
+        return servo;
+    }
+
+    private ServoFTCConfig config(String name) {
+        ServoFTCConfig config = null;
+        assert bot != null;
+        switch (bot) {
+            case FINAL:
+                config = FinalBot(name);
+                break;
+            case CODE:
+                config = CodeBot(name);
+                break;
+        }
+        return config;
+    }
+
+    private static ServoFTCConfig CodeBot(String name) {
+        ServoFTCConfig config = null;
+        switch (name) {
+            default:
+                break;
+        }
+        return config;
+    }
+
+    private static ServoFTCConfig FinalBot(String name) {
+        ServoFTCConfig config = null;
+        switch (name) {
+            case "CLAW-TOP":
+                config = new ServoFTCConfig("CL1", false, 0.0, 0.48);
+                break;
+            case "CLAW-BOTTOM":
+                config = new ServoFTCConfig("CL2", false, 0.09, 0.35);
+                break;
+        }
+        return config;
+    }
+}
