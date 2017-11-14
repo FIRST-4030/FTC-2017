a8c90736543c5a18341649cb0a68280442e0ac1b
Zachary Kotlarek <zach@kotlarek.com>
2017-11-13 19:45:10 -0800

    Tidy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/buttons/ButtonHandlerListener.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/buttons/ButtonHandlerListener.java
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode.buttons;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 public interface ButtonHandlerListener {
-    public void update(boolean pressed);
-    public boolean active();
+    void update(boolean pressed);
+    boolean active();
 }
