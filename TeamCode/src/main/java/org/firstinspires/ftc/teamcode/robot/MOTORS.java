abf840e23a2c41a9726500db2925159f61beff68
Zachary Kotlarek <zach@kotlarek.com>
2017-11-27 16:42:55 -0800

    No more intakes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/MOTORS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/MOTORS.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.robot;
 
 public enum MOTORS {
-    LIFT,
-    INTAKE_LEFT,
-    INTAKE_RIGHT
+    LIFT
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/SERVOS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/SERVOS.java
@@ -2,9 +2,5 @@ package org.firstinspires.ftc.teamcode.robot;
 
 public enum SERVOS {
     CLAW_TOP,
-    CLAW_BOTTOM,
-    INTAKE_LEFT,
-    INTAKE_RIGHT
-//    ,
-//    JEWEL_ARM
+    CLAW_BOTTOM
 }
