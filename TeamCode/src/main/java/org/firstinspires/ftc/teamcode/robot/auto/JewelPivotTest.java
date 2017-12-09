4441cc927a48f912b014e70508b9724877491c1a
robotics <ihsrobotics4030@gmail.com>
2017-12-09 13:56:38 -0800

    better telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/JewelPivotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/JewelPivotTest.java
@@ -140,6 +140,7 @@ public class JewelPivotTest extends OpMode {
 
         // Driver feedback
         telemetry.addData("State", state);
+        telemetry.addData("Running", driver.isRunning(time));
         telemetry.addData("PivotLeft", pivotLeft); // will be false for the first bit
         telemetry.addData("Gyro", robot.gyro.getHeading());
         telemetry.addData("Encoder", robot.wheels.getEncoder());
