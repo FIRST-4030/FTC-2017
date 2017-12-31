63c0b22a511b167e938622d53a9c5dc7b6af4657
Zachary Kotlarek <zach@kotlarek.com>
2017-12-30 18:58:59 -0800

    Calculate Field.AllianceColor.opposite()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/field/Field.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/field/Field.java
@@ -4,5 +4,19 @@ public class Field {
     public static final float MM_PER_INCH = 25.4f;
     public static final int FIELD_WIDTH = (int) ((12 * 12 - 2) * MM_PER_INCH);
 
-    public enum AllianceColor { RED, BLUE }
+    public enum AllianceColor {
+        RED, BLUE;
+
+        public static AllianceColor opposite(AllianceColor color) {
+            switch (color) {
+                case RED:
+                    color = AllianceColor.BLUE;
+                    break;
+                case BLUE:
+                    color = AllianceColor.RED;
+                    break;
+            }
+            return color;
+        }
+    }
 }
